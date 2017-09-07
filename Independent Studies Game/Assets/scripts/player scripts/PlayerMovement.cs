5d5946a80b2931dc2ed27ec1a85c55ebc6a4dc07
Matt Braden <matt.braden73@gmail.com>
2017-09-07 13:21:14 -0400

    Added movement

---
+++ Independent Studies Game/Assets/scripts/player scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//By Matt Braden
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody rb;
+    public float speed;
+    [SerializeField]
+    bool jumpTest;
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        speed = 15;
+        jumpTest = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(0, 0, speed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(-speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(0, 0, -speed));
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&jumpTest)
+        {
+            rb.AddForce(new Vector3(0, 300, 0));
+            jumpTest = false;
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.tag == "Floor") jumpTest = true;
+    }
+}
