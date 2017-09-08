4a1a0e18fc60856076de7b5bc8d5c795ed7e1340
realjaisaka <jaitheboss1@gmail.com>
2017-09-08 19:47:10 -0400

    reeee

--- Independent Studies Game/Assets/scripts/player scripts/CameraMovement.cs
+++ Independent Studies Game/Assets/scripts/player scripts/CameraMovement.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 //by Jai Saka
 public class CameraMovement : MonoBehaviour {
-
+    public GameObject player;
     public GameObject capcam;
     bool ShowCursor;
     [SerializeField]
     float sensitivity;
+    float newrotX;
+    float newrotY;
+    float camrot;
 
     // Use this for initialization
     void Start () {
@@ -17,18 +20,27 @@ public class CameraMovement : MonoBehaviour {
         {
             Cursor.visible = false;
         }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+        }
         Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.mousePresent)
+        newrotX = Input.GetAxis("Mouse X") * sensitivity;
+        newrotY = Input.GetAxis("Mouse Y") * sensitivity;
+        CheckVerticalRotation();
+        capcam.transform.Rotate(-1 * newrotY, 0, 0);
+        gameObject.transform.Rotate(0, newrotX, 0);
+    }
+
+    public void CheckVerticalRotation()
+    {
+        if(Mathf.Abs(capcam.transform.localEulerAngles.x) < -60)
         {
-            float newrotX = Input.GetAxis("Mouse X") * sensitivity;
-            float newrotY = Input.GetAxis("Mouse Y") * sensitivity;
-            transform.Rotate(0, newrotX, 0);
-            capcam.transform.Rotate(-1*newrotY, 0, 0);
-            gameObject.transform.Rotate(0, newrotX, 0);
+            capcam.transform.Rotate(30, 0, 0);
         }
     }
 }
