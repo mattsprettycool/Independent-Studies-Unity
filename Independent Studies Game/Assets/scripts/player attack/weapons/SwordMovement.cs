643e2e99c9b7808b6a9c77b08a26ee482accbcdd
Matt Braden <matt.braden73@gmail.com>
2017-09-09 13:50:55 -0400

    Fixed the sword so it A) doesn't colide w/ terrain and b) syncs w/ the camera instead of the player. I changed the structure of the movement and the camera to accomadate this change.

--- Independent Studies Game/Assets/scripts/player attack/weapons/SwordMovement.cs
+++ Independent Studies Game/Assets/scripts/player attack/weapons/SwordMovement.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class SwordMovement : MonoBehaviour {
 
-    [SerializeField]
-    public GameObject sword;
-    float xpos;
-    float ypos;
-    float zpos;
     bool slashing;
 
 	// Use this for initialization
@@ -18,10 +13,8 @@ public class SwordMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        xpos = sword.transform.position.x;
-        ypos = sword.transform.position.y;
-        zpos = sword.transform.position.z;
-        if (Input.GetMouseButtonDown(0) && slashing != true)
+
+        if (Input.GetMouseButtonDown(0) && !slashing)
         {
             slashing = true;
             StartCoroutine(SlashAndWait(.5f));
@@ -31,8 +24,8 @@ public class SwordMovement : MonoBehaviour {
 
     IEnumerator SlashAndWait(float seconds)
     {
-        sword.transform.Rotate(0, 0, -90);
+        gameObject.transform.Rotate(0, 0, -90);
         yield return new WaitForSeconds(seconds);
-        sword.transform.Rotate(0, 0, 90);
+        gameObject.transform.Rotate(0, 0, 90);
     }
 }
