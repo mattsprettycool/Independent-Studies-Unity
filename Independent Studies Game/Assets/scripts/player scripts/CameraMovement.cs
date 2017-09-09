c99fe1a903717ed6498e66bf144c87e9cc9774e9
Matt Braden <matt.braden73@gmail.com>
2017-09-08 20:00:01 -0400

    Fixed the camera! It's 100% done!

--- Independent Studies Game/Assets/scripts/player scripts/CameraMovement.cs
+++ Independent Studies Game/Assets/scripts/player scripts/CameraMovement.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 //by Jai Saka
 public class CameraMovement : MonoBehaviour {
-    public GameObject player;
+
     public GameObject capcam;
     bool ShowCursor;
     [SerializeField]
     float sensitivity;
-    float newrotX;
-    float newrotY;
-    float camrot;
 
     float newrotX;
     float newrotY;
@@ -25,34 +22,17 @@ public class CameraMovement : MonoBehaviour {
         {
             Cursor.visible = false;
         }
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Cursor.visible = true;
-        }
         Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        newrotX = Input.GetAxis("Mouse X") * sensitivity;
-        newrotY = Input.GetAxis("Mouse Y") * sensitivity;
-        CheckVerticalRotation();
-        capcam.transform.Rotate(-1 * newrotY, 0, 0);
-        gameObject.transform.Rotate(0, newrotX, 0);
-    }
-
-    public void CheckVerticalRotation()
-    {
-        if(Mathf.Abs(capcam.transform.localEulerAngles.x) < -60)
+        if (Input.mousePresent)
         {
-<<<<<<< HEAD
-            capcam.transform.Rotate(30, 0, 0);
-=======
             newrotX += Input.GetAxis("Mouse X") * sensitivity;
             newrotY = Mathf.Min(90, Mathf.Max(-90, newrotY + Input.GetAxis("Mouse Y") * sensitivity));
             capcam.transform.rotation = Quaternion.Euler(-1*newrotY, newrotX , 0);
-            gameObject.transform.rotation = Quaternion.Euler(0, capcam.transform.rotation.y, 0);
->>>>>>> aa3f1f29a188410c8e9b7a5437bb722ccf6a093d
+            gameObject.transform.rotation = Quaternion.Euler(0, newrotX-90, 0);
         }
         if(Input.GetKey(KeyCode.Escape)) Cursor.visible = true;
     }
