659329667a9f350eb6510d3befd0849da5548972
realjaisaka <jaitheboss1@gmail.com>
2017-09-14 22:10:18 -0400

    Started on AI

--- Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
+++ Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 //by Jai Saka
 public class EnemyMovement : MonoBehaviour {
     Rigidbody rb;
     public float speed;
+    public Transform player;
+    NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
-		
+        agent = GetComponent<NavMeshAgent>();
+        speed = 2.5f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        agent.SetDestination(player.position);
+
 	}
 }
--- Independent Studies Game/Assets/scripts/player scripts/PlayerMovement.cs
+++ Independent Studies Game/Assets/scripts/player scripts/PlayerMovement.cs
@@ -10,11 +10,9 @@ public class PlayerMovement : MonoBehaviour {
     float maxVelocity;
     float maxVelSquared;
     public GameObject cameraLoc;
-	PlayerStamina playerStamina;
     bool keyTest;
     // Use this for initialization
     void Start () {
-		playerStamina = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerStamina> ();
         rb = gameObject.GetComponent<Rigidbody>();
         speed = 20;
         maxVelocity = 20;
@@ -60,9 +58,8 @@ public class PlayerMovement : MonoBehaviour {
         {
             rb.AddRelativeForce(new Vector3(0, 0, -speed/2));
         }
-		if (Input.GetKeyDown(KeyCode.Space)&&jumpTest&&playerStamina.currStamina >= 10)
+		if (Input.GetKeyDown(KeyCode.Space)&&jumpTest)
         {
-			playerStamina.currStamina -= 10;
             rb.AddRelativeForce(new Vector3(0, 300, 0));
             jumpTest = false;
         }
