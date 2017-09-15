1898b8224543f59c2453992409756446f920fb01
jaisaka <jaitheboss1@gmail.com>
2017-09-15 10:01:54 -0400

    added basic navmesh, enemy now follows player pretty accurately

--- Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
+++ Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 //by Jai Saka
 public class EnemyMovement : MonoBehaviour {
-    Rigidbody rb;
-    public float speed;
+	public Transform player;
+	NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
-		
+		agent = GetComponent<NavMeshAgent> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		agent.SetDestination (player.position);
 	}
 }
