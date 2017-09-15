5007e43d38b90b5a0455f31bd0152fee3a165c35
Matt Braden <matt.braden73@gmail.com>
2017-09-14 23:00:24 -0400

    changes were made

--- Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
+++ Independent Studies Game/Assets/scripts/enemy scripts/EnemyMovement.cs
@@ -1,23 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 //by Jai Saka
 public class EnemyMovement : MonoBehaviour {
     Rigidbody rb;
     public float speed;
-    public Transform player;
-    NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
-        agent = GetComponent<NavMeshAgent>();
-        speed = 2.5f;
+		
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-        agent.SetDestination(player.position);
-
+	void Update () {
+		
 	}
 }
