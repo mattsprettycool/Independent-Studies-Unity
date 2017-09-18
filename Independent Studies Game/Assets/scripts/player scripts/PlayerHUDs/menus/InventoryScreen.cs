e7a22f768b5ecb1087470fdfa324e5cd9b874271
Matt Braden <matt.braden73@gmail.com>
2017-09-18 13:20:39 -0400

    I commented things and worked on the event trigger - is hard

--- Independent Studies Game/Assets/scripts/player scripts/PlayerHUDs/menus/InventoryScreen.cs
+++ Independent Studies Game/Assets/scripts/player scripts/PlayerHUDs/menus/InventoryScreen.cs
@@ -6,22 +6,30 @@ using System;
 
 public class InventoryScreen : MonoBehaviour {
     bool toggle;
+    //this string outputs every error in the try catch statements
     String errors;
     [SerializeField]
     public Sprite nullImage;
+    //the array of items in the inventory
     [SerializeField]
-    public GameObject item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25, item26, item27, item28, item29;
-    GameObject[] itemArray = new GameObject[30];
+    public GameObject[] itemArray = new GameObject[30];
+    //number of non-null objects in the array
+    int objectsInArray;
     // Use this for initialization
     void Start () {
         toggle = true;
+        //this initially hides the inventory screen
         gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 0);
+        //this makes every single icon clear
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("slot")) obj.GetComponent<Image>().color = new Color(0, 0, 0, 0);
         errors = "";
+        objectsInArray = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        objectsInArray = 0;
+        for (int i = 0; i < 30; i++) if (itemArray[i] != null) objectsInArray++;
         if (Input.GetKeyDown(KeyCode.Tab)&&toggle)
         {
             Time.timeScale = 0f;
@@ -33,7 +41,6 @@ public class InventoryScreen : MonoBehaviour {
             gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 0);
             toggle = true;
         }
-        updateArray();
         if (!toggle)
         {
             showIcons();
@@ -66,38 +73,5 @@ public class InventoryScreen : MonoBehaviour {
             {
                 errors += e;
             }
-    }
-    void updateArray()
-    {
-        itemArray[0] = item0;
-        itemArray[1] = item1;
-        itemArray[2] = item2;
-        itemArray[3] = item3;
-        itemArray[4] = item4;
-        itemArray[5] = item5;
-        itemArray[6] = item6;
-        itemArray[7] = item7;
-        itemArray[8] = item8;
-        itemArray[9] = item9;
-        itemArray[10] = item10;
-        itemArray[11] = item11;
-        itemArray[12] = item12;
-        itemArray[13] = item13;
-        itemArray[14] = item14;
-        itemArray[15] = item15;
-        itemArray[16] = item16;
-        itemArray[17] = item17;
-        itemArray[18] = item18;
-        itemArray[19] = item19;
-        itemArray[20] = item20;
-        itemArray[21] = item21;
-        itemArray[22] = item22;
-        itemArray[23] = item23;
-        itemArray[24] = item24;
-        itemArray[25] = item25;
-        itemArray[26] = item26;
-        itemArray[27] = item27;
-        itemArray[28] = item28;
-        itemArray[29] = item29;
     }
 }
---
+++ Independent Studies Game/Assets/scripts/player scripts/PlayerHUDs/menus/slotSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class slotSelector : EventTrigger {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerEnter()
+    {
+        Debug.Log("yee");
+    }
+}
