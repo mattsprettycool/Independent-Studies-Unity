8d5cbbdf1d39ee028a87d2a445942e4a04e3e985
Matt Braden <matt.braden73@gmail.com>
2017-10-02 13:23:12 -0400

    I got stuck in a glitch, so nothing really got done. I'm working on getting the UI clicking working and it's being mean

--- Independent Studies Game/Assets/scripts/player scripts/PlayerHUDs/menus/slotSelector.cs
+++ Independent Studies Game/Assets/scripts/player scripts/PlayerHUDs/menus/slotSelector.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class slotSelector : MonoBehaviour {
-    public void OnPointerEnter()
+public class slotSelector : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SendYee();
+        Debug.Log(eventData);
+        throw new System.NotImplementedException();
+    }
+
+    public void SendYee()
     {
         Debug.Log("yee");
     }
