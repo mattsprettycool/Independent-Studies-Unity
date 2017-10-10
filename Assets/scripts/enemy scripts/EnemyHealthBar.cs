d7a4a0cd8b848837dc9c60aec454a9b2aa20d6e9
Matt Braden <matt.braden73@gmail.com>
2017-10-10 13:22:51 -0400

    worked on adding an enemy health bar

---
+++ Assets/scripts/enemy scripts/EnemyHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//by Matt Braden
+public class EnemyHealthBar : MonoBehaviour {
+    [SerializeField, Range(0.0f, 1.0f)]
+    float health;
+    float rectHeight;
+    // Use this for initialization
+    void Start()
+    {
+        foreach(GameObject obj in gameObject.GetComponentsInChildren<GameObject>())
+        {
+            //if(obj.tag.Equals("healthBar"))
+
+        }
+        rectHeight = gameObject.GetComponent<RectTransform>().rect.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().rect.width, rectHeight * health);
+    }
+}
