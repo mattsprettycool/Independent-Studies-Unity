e7b4d064d74d908e38f53ea8a26d7c7646b7a2b5
Matt Braden <matt.braden73@gmail.com>
2017-10-11 14:06:15 -0400

    .

--- Assets/scripts/enemy scripts/EnemyHealthBar.cs
+++ Assets/scripts/enemy scripts/EnemyHealthBar.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 //by Matt Braden
 public class EnemyHealthBar : MonoBehaviour {
     [SerializeField, Range(0.0f, 1.0f)]
-    float health;
-    float rectHeight;
+    float health = 1;
+    float rectWidth = 0;
+    GameObject rectangle;
     // Use this for initialization
     void Start()
     {
         foreach(GameObject obj in gameObject.GetComponentsInChildren<GameObject>())
         {
-            //if(obj.tag.Equals("healthBar"))
-
+            if (obj.tag.Equals("healthBar"))
+            {
+                rectWidth = obj.transform.localScale.y;
+                rectangle = obj.gameObject;
+            }
         }
-        rectHeight = gameObject.GetComponent<RectTransform>().rect.height;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(gameObject.GetComponent<RectTransform>().rect.width, rectHeight * health);
+        rectangle.transform.localScale = new Vector3(.9f, rectWidth * health, 1);
     }
 }
