5155e33ee15baf938f03522681526ccf363150e0
Matt Braden <matt.braden73@gmail.com>
2017-10-13 13:07:47 -0400

    I pretty much finalized the head bobbing

--- Assets/scripts/player scripts/HeadBob.cs
+++ Assets/scripts/player scripts/HeadBob.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeadBob : MonoBehaviour {
-	float sineWaveSlice,timer,boobSpeed,boobIncrement,midway,logHorizon,vertical;
-	void Update () {
-		midway = .51f;
+    float sineWaveSlice, timer, midway = .51f, startPosY;
+    GameObject player;
+    [SerializeField]
+    float bobSpeed, bobIncrement;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPosY = player.transform.position.y;
+        Debug.Log(player.transform.position.y);
+    }
+    void Update () {
+		midway = player.transform.position.y + .51f;
 		sineWaveSlice = 0f;
-		logHorizon = Input.GetAxis ("Horizontal");
-		vertical = Input.GetAxis ("Vertical");
-		if (Mathf.Abs (logHorizon) == 0 && Mathf.Abs (vertical) == 0) {
+		if (Mathf.Abs (Input.GetAxis("Horizontal")) == 0 && Mathf.Abs (Input.GetAxis("Vertical")) == 0) {
 			timer = 0;
 		} else {
 			sineWaveSlice = Mathf.Sin (timer);
-			timer += boobSpeed;
+            Debug.Log(sineWaveSlice);
+            timer += bobSpeed;
 			if (timer > Mathf.PI * 2) {
 				timer -= Mathf.PI * 2;
 			}
 		}
 		if (sineWaveSlice != 0) {
-			float translateVal = sineWaveSlice * boobIncrement;
-			float totalAxes = Mathf.Abs (logHorizon) + Mathf.Abs (vertical);
+			float translateVal = sineWaveSlice * bobIncrement;
+			float totalAxes = Mathf.Abs (Input.GetAxis("Horizontal")) + Mathf.Abs (Input.GetAxis("Vertical"));
 			totalAxes = Mathf.Clamp (totalAxes, 0, 1);
 			translateVal = totalAxes * translateVal;
 			float yVal = midway + translateVal;
-			transform.localPosition = new Vector3 (transform.localPosition.x, yVal, transform.localPosition.z);
+			transform.localPosition = new Vector3 (player.transform.position.x, yVal, player.transform.position.z);
 		} else { 
-			transform.localPosition = new Vector3 (transform.localPosition.x, midway, transform.localPosition.z);
+			transform.localPosition = new Vector3 (player.transform.position.x, midway, player.transform.position.z);
 		}
 	}
 }
