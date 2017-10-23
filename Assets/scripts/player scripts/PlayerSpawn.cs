4d7bfca80024e01798a54588ae5d9752ca06d0ed
jaisaka <jaitheboss1@gmail.com>
2017-10-23 10:35:33 -0400

    player now spawns inside of spawn room, spawned to the position of a transform in the array, indexed by an old friend

--- Assets/scripts/player scripts/PlayerSpawn.cs
+++ Assets/scripts/player scripts/PlayerSpawn.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour {
+	public Transform[] spawnPoints;
 	PlayerHealth pHealth;
 	PlayerMovement pMovement;
+	GameObject playerSpawnTrigger;
+	int randy;
 	// Use this for initialization
 	void Start () {
 		pHealth = gameObject.GetComponent<PlayerHealth> ();
 		pMovement = gameObject.GetComponent<PlayerMovement> ();
+		playerSpawnTrigger = GameObject.FindGameObjectWithTag ("PlayerSpawnTrigger");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		playerSpawnTrigger.transform.Rotate (new Vector3 (playerSpawnTrigger.transform.rotation.x, playerSpawnTrigger.transform.rotation.y + 15, playerSpawnTrigger.transform.rotation.z));
 		if (pHealth.currHealth == 0)
 		{
 			pMovement.speed = 0;
-			transform.position = new Vector3 (1, 102, 0);
+			transform.position = new Vector3 (2, 102, 0);
 			pHealth.currHealth = pHealth.startHealth;
 			pMovement.speed = .5f;
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		randy = Random.Range (0, spawnPoints.Length);
+		if (other.tag == "PlayerSpawnTrigger") {
+			transform.position = spawnPoints[randy].position;
 		}
 	}
 }
