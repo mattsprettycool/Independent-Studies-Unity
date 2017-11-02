b2c21943d95030147ac9289754ee401bff08c914
Matt Braden <matt.braden73@gmail.com>
2017-11-02 18:02:16 -0400

    3rd person is finished and activated by pressing v. that's just a debug feature though, but it works well.

--- Assets/scripts/player scripts/CameraMovement.cs
+++ Assets/scripts/player scripts/CameraMovement.cs
@@ -10,12 +10,19 @@ public class CameraMovement : MonoBehaviour {
     float newrotY;
     [SerializeField]
     bool isFirstPerson = true;
+    GameObject thirdCamSpot, thirdCamSpotPos;
+    Transform thirdCS;
+    float thirdY;
     // Use this for initialization
     void Start () {
         sensitivity = 4.0f;
         newrotX=0;
         newrotY=0;
-	}
+        thirdCamSpot = GameObject.FindGameObjectWithTag("3ps");
+        thirdCS = thirdCamSpot.transform;
+        thirdCamSpotPos = GameObject.FindGameObjectWithTag("3pspos");
+        thirdY = 0;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -40,6 +47,25 @@ public class CameraMovement : MonoBehaviour {
                 newrotX += Input.GetAxis("Mouse X") * sensitivity;
 
                 newrotY = Mathf.Min(90, Mathf.Max(-90, newrotY + Input.GetAxis("Mouse Y") * sensitivity));
+
+                capcam.transform.rotation = Quaternion.Euler(-1 * newrotY, newrotX, 0);
+
+                gameObject.transform.rotation = Quaternion.Euler(0, newrotX - 90, 0);
+            }
+        }
+        else
+        {
+            if (Input.mousePresent)
+            {
+                newrotX += Input.GetAxis("Mouse X") * sensitivity;
+
+                if(thirdY<=0&& Input.GetAxis("Mouse Y") >= 0)
+                    thirdY += Input.GetAxis("Mouse Y") * .05f;
+                if (Input.GetAxis("Mouse Y") <= 0 && thirdY >= -.25f)
+                    thirdY += Input.GetAxis("Mouse Y") * .05f;
+                thirdCamSpot.transform.position = new Vector3(thirdCS.position.x, thirdY+thirdCamSpotPos.transform.position.y, thirdCS.position.z);
+
+                newrotY = Mathf.Min(40, Mathf.Max(-60, newrotY + Input.GetAxis("Mouse Y") * sensitivity));
 
                 capcam.transform.rotation = Quaternion.Euler(-1 * newrotY, newrotX, 0);
 
--- Assets/scripts/player scripts/HeadBob.cs
+++ Assets/scripts/player scripts/HeadBob.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 //by Someone
 public class HeadBob : MonoBehaviour {
     float sineWaveSlice, timer, midway = .51f;
-    GameObject player;
+    GameObject player, cameraLoc;
     [SerializeField]
     float bobSpeed, bobIncrement = 0;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        cameraLoc = GameObject.FindGameObjectWithTag("ItemBarHolder");
     }
     void Update()
     {
         if (Time.timeScale == 1f)
         {
+            GameObject camLoc = GameObject.FindGameObjectWithTag("camRef");
+            gameObject.transform.position = new Vector3(camLoc.transform.position.x, camLoc.transform.position.y, camLoc.transform.position.z);
             midway = player.transform.position.y + .51f;
             sineWaveSlice = 0f;
             if (Mathf.Abs(Input.GetAxis("Horizontal")) == 0 && Mathf.Abs(Input.GetAxis("Vertical")) == 0)
@@ -39,12 +42,12 @@ public class HeadBob : MonoBehaviour {
                 totalAxes = Mathf.Clamp(totalAxes, 0, 1);
                 translateVal = totalAxes * translateVal;
                 float yVal = midway + translateVal;
-                transform.localPosition = new Vector3(Camera.main.transform.position.x, yVal, Camera.main.transform.position.z);
+                transform.localPosition = new Vector3(cameraLoc.transform.position.x, yVal, cameraLoc.transform.position.z);
                 transform.rotation = Camera.main.transform.rotation;
             }
             else
             {
-                transform.localPosition = new Vector3(Camera.main.transform.position.x, midway, Camera.main.transform.position.z);
+                transform.localPosition = new Vector3(cameraLoc.transform.position.x, midway, cameraLoc.transform.position.z);
                 transform.rotation = Camera.main.transform.rotation;
             }
         }
