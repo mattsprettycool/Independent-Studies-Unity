8b5b888ef696c6ba031bf07680fb245d8403a884
Matt Braden <matt.braden73@gmail.com>
2017-11-06 13:16:30 -0500

    I started work on a  M A G I C  sword

---
+++ Assets/scripts/player scripts/player attack/spells/ProjectileSword.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSword : MonoBehaviour {
+    //TODO: have 3rd person show when equipted
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
