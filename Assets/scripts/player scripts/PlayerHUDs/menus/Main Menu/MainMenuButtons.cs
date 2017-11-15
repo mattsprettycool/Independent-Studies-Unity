056014f7960b80c693fa3a6b7194ff8d38eef331
Matt Braden <matt.braden73@gmail.com>
2017-11-15 13:11:38 -0500

    I'm working on the main menu but my computer isn't being nice right now

---
+++ Assets/scripts/player scripts/PlayerHUDs/menus/Main Menu/MainMenuButtons.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour {
+    public void EndGame()
+    {
+        Application.Quit();
+        Debug.Log("yee");
+    }
+}
