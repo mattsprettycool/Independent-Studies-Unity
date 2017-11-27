2132b1884910fb1bbbbf2f30d80979cf2b97c75a
Matt Braden <matt.braden73@gmail.com>
2017-11-27 13:14:20 -0500

    The fireball shows up again and I made the explosion alot smaller and removed the extra damage you gave it.

--- Assets/scripts/player scripts/player attack/spells/FireboltExplosion.cs
+++ Assets/scripts/player scripts/player attack/spells/FireboltExplosion.cs
@@ -6,13 +6,5 @@ public class FireboltExplosion : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         gameObject.GetComponent<ParticleSystem>().Play();
-		GameObject.Destroy(gameObject, gameObject.GetComponent<ParticleSystem>().main.duration);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Enemies")
-        {
-            other.GetComponent<EnemyHealth>().TakeDamage(10);
-        }
     }
 }
