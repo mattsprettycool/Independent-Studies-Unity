4fd3bb1c68ff7c30edfd154555839dcc5007a801
Matt Braden <matt.braden73@gmail.com>
2017-12-08 13:25:13 -0500

    I started work on a telekinesis spell because I was inspired by mob pyscho

---
+++ Assets/scripts/player scripts/player attack/spells/Telekinesis.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telekinesis : MonoBehaviour {
+    GameObject currentlySelectedEnemy;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //debug
+            Destroy(GetNearestEnemy(25));
+        }
+	}
+
+    GameObject GetNearestEnemy(float maxDistance)
+    {
+        GameObject nearestObj = null;
+        float closestEnemy = float.MaxValue;
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Enemies"))
+        {
+            Debug.Log(GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z));
+            if ((GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) < closestEnemy) && (GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) <= maxDistance))
+                nearestObj = obj;
+        }
+        
+        return nearestObj;
+    }
+
+    float GetDistance(float myX, float myZ, float theirX, float theirZ)
+    {
+        return Mathf.Sqrt(Mathf.Pow((myX+theirX),2)+ Mathf.Pow((myZ + theirZ), 2));
+    }
+}
