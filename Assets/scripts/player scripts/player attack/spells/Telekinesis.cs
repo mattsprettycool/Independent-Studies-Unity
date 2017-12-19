71b8e7c9abb3587441c3fbb52f0bb344952b509c
Matt Braden <matt.braden73@gmail.com>
2017-12-19 13:34:52 -0500

    ayo I'm back and doing some more stuff

--- Assets/scripts/player scripts/player attack/spells/Telekinesis.cs
+++ Assets/scripts/player scripts/player attack/spells/Telekinesis.cs
@@ -5,7 +5,6 @@ using UnityEngine.AI;
 
 public class Telekinesis : MonoBehaviour {
     Transform currentlySelectedEnemy;
-    bool enemySelected = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,47 +14,27 @@ public class Telekinesis : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (!enemySelected)
-            {
-                RaycastHit hitInfo;
-                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward * 1000, out hitInfo, 20, 1 << 9))
-                {
-                    if (hitInfo.collider.tag.Equals("Enemies"))
-                    {
-                        currentlySelectedEnemy = hitInfo.collider.GetComponent<Transform>();
-                        enemySelected = true;
-                    }
-                }
-            }
-            else
-            {
-                currentlySelectedEnemy.GetComponent<NavMeshAgent>().velocity = new Vector3(100, 0, 0);
-                enemySelected = false;
-            }
-        }
-        if (enemySelected)
-        {
-            GameObject telePointer = GameObject.FindGameObjectWithTag("tloc");
-            currentlySelectedEnemy.position = new Vector3(telePointer.transform.position.x, telePointer.transform.position.y, telePointer.transform.position.z);
+            currentlySelectedEnemy = GetNearestEnemy(100).transform;
+            currentlySelectedEnemy.GetComponent<NavMeshAgent>().velocity = Vector3.Normalize(transform.position)*100;
         }
     }
 
-    //GameObject GetNearestEnemy(float maxDistance)
-    //{
-     //   GameObject nearestObj = null;
-     //   float closestEnemy = float.MaxValue;
-     //   foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Enemies"))
-     //   {
-     //       Debug.Log(GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z));
-     //       if ((GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) < closestEnemy) && (GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) <= maxDistance))
-     //           nearestObj = obj;
-     //   }
+    GameObject GetNearestEnemy(float maxDistance)
+    {
+        GameObject nearestObj = null;
+        float closestEnemy = float.MaxValue;
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Enemies"))
+        {
+            Debug.Log(GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z));
+            if ((GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) < closestEnemy) && (GetDistance(transform.position.x, transform.position.z, obj.transform.position.x, obj.transform.position.z) <= maxDistance))
+                nearestObj = obj;
+        }
         
-      //  return nearestObj;
-    //}
+        return nearestObj;
+    }
 
-    //float GetDistance(float myX, float myZ, float theirX, float theirZ)
-    //{
-    //    return Mathf.Sqrt(Mathf.Pow((myX+theirX),2)+ Mathf.Pow((myZ + theirZ), 2));
-    //}
+    float GetDistance(float myX, float myZ, float theirX, float theirZ)
+    {
+        return Mathf.Sqrt(Mathf.Pow((myX+theirX),2)+ Mathf.Pow((myZ + theirZ), 2));
+    }
 }
