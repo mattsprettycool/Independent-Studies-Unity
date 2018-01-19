660c84555c82bae4b0ff01aebf789483fdc78ae5
Matt Braden <matt.braden73@gmail.com>
2018-01-19 13:23:21 -0500

    Just working on time stop spell

---
+++ Assets/scripts/player scripts/player attack/spells/TimeStopSpell.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStopSpell : MonoBehaviour {
+    ArtificialTimeManager realTime;
+    bool timeOn;
+    SpellManager spellMan;
+    float timeForSpell = 100, currTime = 0;
+    [SerializeField]
+    GameObject timeDrop;
+	// Use this for initialization
+	void Start () {
+        spellMan = gameObject.GetComponent<SpellManager>();
+        realTime = GameObject.FindGameObjectWithTag("Player").GetComponent<ArtificialTimeManager>();
+        timeOn = realTime.IsTimeOn();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var i = Instantiate(timeDrop);
+            i.transform.position = gameObject.transform.position;
+            i.transform.rotation = gameObject.transform.rotation;
+        }
+        if (!timeOn)
+        {
+            if (currTime < timeForSpell)
+            {
+                currTime++;
+            }
+            else
+            {
+                timeOn = true;
+                currTime = 0;
+            }
+        }
+        realTime.SetTime(timeOn);
+	}
+}
