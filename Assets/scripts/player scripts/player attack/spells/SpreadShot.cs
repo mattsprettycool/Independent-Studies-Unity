16b583c2ce90a8af3495fd889b15cbe02d8d3940
jaisaka <jaitheboss1@gmail.com>
2018-03-19 12:17:48 -0400

    removed shadown bois

--- Assets/scripts/player scripts/player attack/spells/SpreadShot.cs
+++ Assets/scripts/player scripts/player attack/spells/SpreadShot.cs
@@ -19,7 +19,7 @@ public class SpreadShot : MonoBehaviour {
 		if (Input.GetKeyUp (KeyCode.Mouse0) && spm.HasManaNumber () && Time.timeScale == 1f) {
 			for (int i = 0; i < 12; i++){
 				GameObject proj = Instantiate (projectile, gameObject.transform);
-				proj.GetComponent<Transform> ().transform.localScale = new Vector3 (.001f * size, .001f * size, .001f * size);
+				proj.GetComponent<Transform> ().transform.localScale = new Vector3 (.01f * size, .01f * size, .01f * size);
 				proj.GetComponent<Transform> ().Rotate (Random.Range (0, 15), Random.Range (0, 20), 0);
 				proj.GetComponent<Transform> ().SetParent (null);
 			}spm.LoseMana ();
