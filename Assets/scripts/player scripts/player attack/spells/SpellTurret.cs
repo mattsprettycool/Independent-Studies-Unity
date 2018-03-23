0be4b91271a024ce6ebefd50b0853eaa82ffbbc1
Matt Braden <matt.braden73@gmail.com>
2018-03-23 12:18:01 -0400

    a

--- Assets/scripts/player scripts/player attack/spells/SpellTurret.cs
+++ Assets/scripts/player scripts/player attack/spells/SpellTurret.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpellTurret : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    float timeUntilStart = 0;
+    float curTime = 0;
+    [SerializeField]
+    GameObject projectile;
+    private void Start()
+    {
+        curTime = timeUntilStart;
+    }
+    private void FixedUpdate()
+    {
+        if (curTime > 0)
+        {
+            curTime--;
+        }
+        else
+        {
+            ShootEnemy();
+        }
+    }
+    void ShootEnemy()
+    {
+        var proj = Instantiate(projectile);
+        proj.transform.position = GameObject.FindGameObjectWithTag("Enemies").GetComponent<Transform>().position;
+        AddCooldown(250);
+    }
+    void AddCooldown(float x)
+    {
+        curTime += x;
+    }
 }
