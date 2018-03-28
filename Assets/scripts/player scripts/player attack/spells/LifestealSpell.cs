03db5e6c4bea754ce1b21bbf6da7ddbbeb2a4317
Matt Braden <matt.braden73@gmail.com>
2018-03-28 11:46:36 -0400

    I added edge

---
+++ Assets/scripts/player scripts/player attack/spells/LifestealSpell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifestealSpell : MonoBehaviour {
+    LineRenderer lr;
+    GameObject player;
+    SpellManager spm;
+	// Use this for initialization
+	void Start () {
+        spm = gameObject.GetComponent<SpellManager>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        lr = gameObject.GetComponentInChildren<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lr.transform.rotation = Quaternion.LookRotation(Vector3.zero, Vector3.zero);
+        lr.transform.position = player.transform.position;
+        if (Input.GetMouseButton(0) && spm.HasManaNumber())
+        {
+            spm.LoseMana();
+            spm.SetManaCooldown();
+            GameObject enemy = GameObject.FindGameObjectWithTag("Enemies");
+            lr.SetPosition(1, new Vector3(enemy.transform.position.x - player.transform.position.x, enemy.transform.position.y - player.transform.position.y, enemy.transform.position.z - player.transform.position.z));
+        }else
+            lr.SetPosition(1, new Vector3(0, 0, 0));
+    }
+}
