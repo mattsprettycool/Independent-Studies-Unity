445cd3c6e89e008f03d6dd9c41e43b779b0fe68e
Matt Braden <matt.braden73@gmail.com>
2018-04-13 12:04:50 -0400

    OwU

--- Assets/scripts/player scripts/player attack/spells/LifestealSpell.cs
+++ Assets/scripts/player scripts/player attack/spells/LifestealSpell.cs
@@ -6,8 +6,11 @@ public class LifestealSpell : MonoBehaviour {
     LineRenderer lr;
     GameObject player;
     SpellManager spm;
-	// Use this for initialization
-	void Start () {
+    float damagePercentage = .0025f;
+    PlayerHealth playerHealth;
+    // Use this for initialization
+    void Start () {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         spm = gameObject.GetComponent<SpellManager>();
         player = GameObject.FindGameObjectWithTag("Player");
         lr = gameObject.GetComponentInChildren<LineRenderer>();
@@ -23,6 +26,11 @@ public class LifestealSpell : MonoBehaviour {
             spm.SetManaCooldown();
             GameObject enemy = GameObject.FindGameObjectWithTag("Enemies");
             lr.SetPosition(1, new Vector3(enemy.transform.position.x - player.transform.position.x, enemy.transform.position.y - player.transform.position.y, enemy.transform.position.z - player.transform.position.z));
+            float enemyHealth = enemy.GetComponent<EnemyHealth>().startHealth;
+            float currDamage = enemyHealth * damagePercentage;
+            enemy.GetComponent<EnemyHealth>().TakeDamage(currDamage);
+            if(playerHealth.currHealth < 100)
+                playerHealth.currHealth += currDamage;
         }else
             lr.SetPosition(1, new Vector3(0, 0, 0));
     }
