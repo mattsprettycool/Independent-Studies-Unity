79bf8de07b4160a5d4aa64b003d0b2854166ad9d
jaisaka <jaitheboss1@gmail.com>
2018-04-19 11:36:55 -0400

    heck yeck

--- Assets/scripts/player scripts/player attack/spells/SpreadShot.cs
+++ Assets/scripts/player scripts/player attack/spells/SpreadShot.cs
@@ -6,7 +6,6 @@ public class SpreadShot : MonoBehaviour {
 	SpellManager spm;
 	PlayerMana pMana;
 	public GameObject projectile;
-	float size;
 	// Use this for initialization
 	void Start () {
 		spm = gameObject.GetComponent<SpellManager> ();
@@ -15,14 +14,15 @@ public class SpreadShot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKeyDown (KeyCode.Mouse0) && spm.HasManaNumber () && Time.timeScale == 1f && spm.IsOffCooldown()) {
+		if (Input.GetMouseButtonDown(0) && spm.HasManaNumber () && Time.timeScale == 1f && spm.IsOffCooldown()) {
+			spm.LoseMana (); spm.SetManaCooldown(); spm.SetSpellCooldown ();
 			for (int i = 0; i < 12; i++){
 				GameObject proj = Instantiate (projectile, gameObject.transform);
 				proj.GetComponent<Transform> ().transform.localScale = new Vector3 (.1f, .1f, .1f);
 				proj.GetComponent<Transform> ().localPosition = new Vector3 (0, 0, 0);
 				proj.GetComponent<Transform> ().Rotate (Random.Range (-7.5f, 7.5f), Random.Range (-7.5f, 7.5f), 0);
 				proj.GetComponent<Transform> ().SetParent (null);
-			}spm.LoseMana (); spm.SetManaCooldown(); size = 0;
+			}
 		}
 		if (spm.IsOffCooldown())
 		{
