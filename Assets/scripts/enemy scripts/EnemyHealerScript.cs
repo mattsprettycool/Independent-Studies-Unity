0693e69ab51a57a7b49b88bd928b04b4fc776bf7
jaisaka <jaitheboss1@gmail.com>
2018-04-24 12:17:48 -0400

    finished healer per se, will probably switch to raycasting to avoid lag

--- Assets/scripts/enemy scripts/EnemyHealerScript.cs
+++ Assets/scripts/enemy scripts/EnemyHealerScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EnemyHealerScript : MonoBehaviour {
 	void OnTriggerStay (Collider col){
-		if (col.tag == "Enemies" && col.GetComponent<EnemyHealth>().currHealth < 100) {
-			col.GetComponent<EnemyHealth> ().currHealth += .001f;						
+		if (col.tag == "Enemies" && col.GetComponent<EnemyHealth>().currHealth < col.GetComponent<EnemyHealth>().startHealth && col.gameObject != this.gameObject.GetComponentInParent<Transform>().gameObject) {
+			Debug.Log ("Healing: " + col.gameObject.transform.position + "Target has: " +  col.GetComponent<EnemyHealth>().currHealth + " health");
+			col.GetComponent<EnemyHealth> ().currHealth += .01f;						
 		}
 	}
 }
