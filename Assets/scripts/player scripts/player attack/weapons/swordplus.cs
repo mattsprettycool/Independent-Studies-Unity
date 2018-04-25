f839beb1242ab60ecc37917b9ebae302bc3f585a
Matt Braden <matt.braden73@gmail.com>
2018-04-25 12:17:55 -0400

    I made a better sword

--- Assets/scripts/player scripts/player attack/weapons/swordplus.cs
+++ Assets/scripts/player scripts/player attack/weapons/swordplus.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class swordplus : MonoBehaviour {
     Animator anim;
-    int maxAngle = 60;
-    float range = 10;
+    PlayerStamina playerStamina;
+    int maxAngle = 10;
+    float range = 5;
+    int damage = 10;
     private void Start()
     {
         anim = gameObject.GetComponentInChildren<Animator>();
+        playerStamina = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStamina>();
     }
     void Update () {
         if (Input.GetMouseButton(0))
         {
-            if (anim.GetCurrentAnimatorStateInfo(0).IsName("swingsword"))
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("swingsword")&&playerStamina.currStamina>0)
             {
+                playerStamina.currStamina -= 5;
                 AttackAllInRange();
                 anim.Play("SwordSwing1");
             }
@@ -23,26 +27,18 @@ public class swordplus : MonoBehaviour {
     void AttackAllInRange()
     {
         Ray rangeRay = new Ray(RemoveY(Camera.main.transform.position), RemoveY(Camera.main.transform.forward));
-        //Debug.DrawRay(RemoveY(Camera.main.transform.position), RemoveY(Camera.main.transform.forward), Color.blue, 10000000);
+        Debug.DrawRay(RemoveY(Camera.main.transform.position), RemoveY(Camera.main.transform.forward), Color.blue, 10000000);
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemies"))
         {
             Ray enemyRay = new Ray(RemoveY(Camera.main.transform.position), RemoveY(GetRelativeVector(Camera.main.transform.position, obj.transform.position)));
             //Debug.DrawRay(RemoveY(Camera.main.transform.position), RemoveY(GetRelativeVector(Camera.main.transform.position, obj.transform.position)), Color.red, 10000000);
-            //Next thing I need to do is get the ray to register if it is close - or maybe start with the angle stuff because it is easier
+            if(GetDistance(enemyRay.GetPoint(0), obj.transform.position) <= range)
+            {
+                Debug.Log(GetAngleFromRays(rangeRay, enemyRay));
+                if(GetAngleFromRays(rangeRay, enemyRay)<=maxAngle)
+                    obj.GetComponent<EnemyHealth>().TakeDamage(damage);
+            }
         }
-        //foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Enemies"))
-        //{
-        //RaycastHit hit;
-        //if (Physics.Raycast(Camera.main.transform.position, obj.transform.position, out hit))
-        //{
-        //Vector2 point1 = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.z);
-        //Vector2 point2 = new Vector2(obj.transform.position.x, obj.transform.position.z);
-        //Vector2 point3 = new Vector2()
-        //if (GetDistance(transform.position, obj.transform.position) <= 10 && (hit.normal.x/ Mathf.Abs(hit.normal.x)) == (Camera.main.transform.rotation.x / Mathf.Abs(Camera.main.transform.rotation.x))) {
-        //obj.GetComponent<EnemyHealth>().TakeDamage(25);
-        //}
-        //}
-        //}
     }
     float GetDistance(Vector3 x, Vector3 y)
     {
@@ -55,5 +51,9 @@ public class swordplus : MonoBehaviour {
     Vector3 GetRelativeVector(Vector3 newOrigin, Vector3 otherPoint)
     {
         return new Vector3(otherPoint.x - newOrigin.x, otherPoint.y - newOrigin.y, otherPoint.z - newOrigin.z);
+    }
+    float GetAngleFromRays(Ray relativeOrigin, Ray angleRay)
+    {
+        return Vector3.Angle(angleRay.GetPoint(range), relativeOrigin.GetPoint(range));
     }
 }
