e9c49dea7b7e97a4a0f78d2055db5779c4611501
jaisaka <jaitheboss1@gmail.com>
2018-05-03 11:44:21 -0400

    FUCK

--- Assets/scripts/player scripts/player attack/spells/PortalGunScript.cs
+++ Assets/scripts/player scripts/player attack/spells/PortalGunScript.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class PortalGunScript : MonoBehaviour {
 	public GameObject portal;
+	SpellManager spm;
+	GameObject player;
+	GameObject spawnedPortal;
 	// Use this for initialization
 	void Start () {
-		
+		spm = gameObject.GetComponent<SpellManager> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		/*when player left clicks, raycast a point on the ground, spawn portal on it if there is not already a portal on the map
 		 *If there is a portal on the map, send the player to it
 		 *when the player right clicks, destroy whatever portal is already on the map*/
-		if (Input.GetMouseButtonDown (0)) {
+		if (Input.GetMouseButtonDown (0) && GameObject.FindGameObjectWithTag("Portal") == null && spm.IsOffCooldown()) {
 			RaycastHit hit;
 			if (Physics.Raycast(transform.position, Vector3.forward, out hit, 20)){
-				Instantiate(portal, hit.point, hit.transform.rotation);
+				Instantiate(portal, hit.point, Quaternion.Euler(new Vector3 (90, 0, 0)));
 			}
 		}
+		if (Input.GetMouseButtonDown (0) && !(GameObject.FindGameObjectWithTag ("Portal") == null) && spm.IsOffCooldown()) {
+			player.transform.position = GameObject.FindGameObjectWithTag ("Portal").transform.position;
+		}
+		if (Input.GetMouseButtonDown (1) && !(GameObject.FindGameObjectWithTag ("Portal") == null)) {
+			GameObject.Destroy (GameObject.FindGameObjectWithTag ("Portal"));
+		}
+		if (spm.IsOffCooldown())
+		{
+			gameObject.GetComponent<attackLibrary>().SetSavedValuesAtSpot(1, 1);
+		}
+		else
+			gameObject.GetComponent<attackLibrary>().SetSavedValuesAtSpot(1, 0);
+		gameObject.GetComponent<attackLibrary>().SetSavedValuesAtSpot(0, spm.GetCooldownRatio());
 	}
 }
